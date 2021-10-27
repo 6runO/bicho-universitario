8894defea4ed5a0b20616a0dd8071985dc060c1a
Leonardo Rodrigues <leonardomyfiis@gmail.com>
2021-10-27 19:57:15 -0300

    Criado metodos new e create bicho, e bicho view

--- app/controllers/bichos_controller.rb
+++ app/controllers/bichos_controller.rb
@@ -1,2 +1,20 @@
 class BichosController < ApplicationController
+  def new
+    @bicho = Bicho.new
+  end
+
+  def create
+    @bicho = Bicho.new(bicho_params)
+    if @bicho.save
+      redirect_to bicho_path(@bicho)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bicho_params
+    params.require(:bicho).permit(:apelido, :cor, :porte, :sexo)
+  end
 end
