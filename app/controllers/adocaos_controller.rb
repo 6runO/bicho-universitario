8a4df7ca3ff370f117960e5bd60912a81ee2c4f7
Leonardo Rodrigues <leonardomyfiis@gmail.com>
2021-10-29 12:24:58 -0300

    Criado metodo create da adocao

--- app/controllers/adocaos_controller.rb
+++ app/controllers/adocaos_controller.rb
@@ -1,2 +1,8 @@
 class AdocaosController < ApplicationController
+  def create
+    @adocao = Adocao.new
+    @bicho = Bicho.find(params[:bicho_id])
+    @adocao.bicho = @bicho
+    @adocao.save
+  end
 end
