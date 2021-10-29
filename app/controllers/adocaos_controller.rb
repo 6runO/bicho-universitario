7d5c49f3f468e6a789653a8a189860f68895d6a9
Leonardo Rodrigues <leonardomyfiis@gmail.com>
2021-10-29 13:05:20 -0300

    Complementando método create de adocao

--- app/controllers/adocaos_controller.rb
+++ app/controllers/adocaos_controller.rb
@@ -1,8 +1,14 @@
 class AdocaosController < ApplicationController
   def create
-    @adocao = Adocao.new
+    @adocao = Adocao.new(adocao_params)
     @bicho = Bicho.find(params[:bicho_id])
     @adocao.bicho = @bicho
     @adocao.save
+  end
+
+  private
+
+  def adocao_params
+    params.require(:adocao).permit(:bicho_id, :user_id)
   end
 end
