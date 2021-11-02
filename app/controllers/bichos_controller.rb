eff545764c0b7ed711fe15c1cc1babbb346ff6ca
bruno gonzaga <babsgonzaga@gmail.com>
2021-11-02 17:26:12 -0300

    Autorizações implementadas.

--- app/controllers/bichos_controller.rb
+++ app/controllers/bichos_controller.rb
@@ -1,4 +1,5 @@
 class BichosController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_bicho, only: [:show, :destroy, :update, :edit]
 
   def index
